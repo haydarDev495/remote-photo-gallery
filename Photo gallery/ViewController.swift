b5f4db3bd51e89ed8e9d1c7464f091e13034b8da
Haydar Bekmuradov <haydarbekmuradov50@gmail.com>
2022-05-25 11:46:45 +0300

    Initial Commit

---
+++ Photo gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo gallery
+//
+//  Created by Haydar Bekmuradov on 25.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
